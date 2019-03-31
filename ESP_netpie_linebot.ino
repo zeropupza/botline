4ada47d63a186222178c91c73b6511a751b7e588
zeropupza <49114997+zeropupza@users.noreply.github.com>
2019-03-31 16:39:55 +0700

    Update ESP_netpie_linebot.ino

--- ESP_netpie_linebot.ino
+++ ESP_netpie_linebot.ino
@@ -5,10 +5,10 @@
 const char* ssid     = "your SSID"; //change this to your SSID
 const char* password = "your PASSWORD"; //change this to your PASSWORD
 
-const char* host = "your linebot server";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
-#define APPID   "your APPID"     //change this to your APPID
-#define KEY     "your KEY"     //change this to your KEY
-#define SECRET  "your SECRET"     //change this to your SECRET
+const char* host = "https://botlinefeeder.herokuapp.com/bot.php";//change this to your linebot server ex.http://numpapick-linebot.herokuapp.com/bot.php
+#define APPID   "botlinefeeder"     //change this to your APPID
+#define KEY     "Rdevc7rR9enHouq"     //change this to your KEY
+#define SECRET  "REOZ8LFanWY6fv0lLsOjMIfNd"     //change this to your SECRET
 
 #define ALIAS   "NodeMCU1" //set name of drvice
 #define TargetWeb "switch" //set target name of web
